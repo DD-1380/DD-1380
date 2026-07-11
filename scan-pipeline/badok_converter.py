7d3f4c1be47112327a895136d739b41aebd606e6
Angel Wu <simon.starhive@protom.me>
2026-07-11 01:42:33 -0400

    added the abttraction

--- scan-pipeline/badok_converter.py
+++ scan-pipeline/badok_converter.py
@@ -3,3 +3,22 @@ import json
 
 with open ("field_map.json", "r") as f:
     FIELD_MAP = json.load(f)
+
+def convert_to_badok(
+    extracted_fields: Dict[str,Any],
+    document_id: int | None = None;
+) -> Dict[str,Any]:
+    values = {}
+
+    for internal_name, value in extracted_fields.items():
+
+        badok_name = FIELD_MAP.get(internal_name)
+
+        if badok_name is None:
+            continue
+
+        values[badok_name] = value
+
+    return{
+    }
+
